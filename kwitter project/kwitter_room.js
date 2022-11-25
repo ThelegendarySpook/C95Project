cefdd313cd263e21713cff1b1545c6ff8b7c2369
ThelegendarySpook <115424068+ThelegendarySpook@users.noreply.github.com>
2022-11-25 08:35:46 +0100

    Add files via upload

---
+++ kwitter project/kwitter_room.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCcI88D8PW8fky37P0NdL244OeHQb7MbzA",
+    authDomain: "kwitter-knock-of-twitter.firebaseapp.com",
+    databaseURL: "https://kwitter-knock-of-twitter-default-rtdb.firebaseio.com",
+    projectId: "kwitter-knock-of-twitter",
+    storageBucket: "kwitter-knock-of-twitter.appspot.com",
+    messagingSenderId: "313453830145",
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//ADD YOUR FIREBASE LINKS
